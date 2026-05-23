1b22b5da12a870fc9d8e9f6f67d7512193926f48
Амир_Шавалиев <amir230807@gmail.com>
2026-05-24 01:06:09 +0300

    Add Test StacInitList

--- test/test_Stack.cpp
+++ test/test_Stack.cpp
@@ -37,3 +37,31 @@ TEST(StackBasic, Size){
     s.push(2);
     EXPECT_EQ(s.size(), 2);
 }
+
+TEST(StackInitList, Size){
+    Stack<MutableSequenceArray, int> s {1,2,3,4};
+    EXPECT_EQ(s.size(), 4);
+    s.push(5);
+    EXPECT_EQ(s.size(), 5);
+}
+
+TEST(StackInitList, Empty){
+    Stack<MutableSequenceArray, int> s {1,2,3,4};
+    EXPECT_FALSE(s.empty());
+}
+
+TEST(StackInitList, PushandPop){
+    Stack<MutableSequenceArray, int> s {1,2,3,4};
+    EXPECT_EQ(s.pop(), 4);
+    EXPECT_EQ(s.pop(), 3);
+    EXPECT_EQ(s.pop(), 2);
+}
+
+TEST(StackInitList, PushandTop){
+    Stack<MutableSequenceArray, int> s {1,2,3,4};
+    EXPECT_EQ(s.top(), 4);
+    s.pop();
+    EXPECT_EQ(s.top(), 3);
+    s.pop();
+    EXPECT_EQ(s.top(), 2);
+}
